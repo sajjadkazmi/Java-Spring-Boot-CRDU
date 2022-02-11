30c4bf55312b19d9bb2947dbd94f480806b21ad6
sajjad.kazmi <sajjad.kazmi@regex.global>
2022-02-11 10:36:06 +0500

    project setup

---
+++ src/main/java/com/regexlms/regexlms/RegexlmsApplication.java
@@ -0,0 +1,13 @@
+package com.regexlms.regexlms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegexlmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegexlmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/regexlms/regexlms/RegexlmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.regexlms.regexlms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegexlmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
